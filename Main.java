bcc4f1fca7a751ba6d8e3236c9fc51a11c7ddc28
Tabrez-N <tabrez.n2023@vitstudent.ac.in>
2026-01-12 23:04:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Student Name: Tabrez.N");
+        System.out.println("Register Number: 23MIC0112");
+        System.out.println("Message: Jenkins-GitHub Integration Successful");
+    }
+}
